deb0b8dcf0d8258ba7c54b3b699b6bb7f4c4d72f
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2019-08-03 12:26:35 -0400

    Base coed

---
+++ src/components/AddBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="bar-area">
+      <div class="bar-container">
+          <input type="text" placeholder="Eggs" autofocus v-model="message">
+      </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'AddBar',
+        computed: {
+            message: {
+                get () {
+                    return this.$store.state.inputText;
+                },
+                set (value) {
+                    this.$store.commit('changeInputText', value)
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .bar-area {
+        width: 100%;
+        display: block;
+        height: 400px;
+        background-color: lightsteelblue;
+        padding: 170px 0px;
+        box-sizing: border-box; 
+    }
+
+    input {
+        width: 500px;
+        height: 60px;
+        box-sizing: border-box;
+        border: 0px;
+        font-size: 23px;
+        padding: 0px 40px;
+        border-radius: 50px;
+    }
+
+    input:focus {
+        outline: none;
+    }
+</style>
--- src/store.js
+++ src/store.js
@@ -5,12 +5,11 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    inputText: "",
   },
   mutations: {
-
-  },
-  actions: {
-
+    changeInputText (state, newText) {
+      state.inputText = newText;
+    }
   }
 })
